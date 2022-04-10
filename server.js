209536e0ad701471d586c78ec9b26e09ec3ea4c7
Taras Zakharko <taras05101996@i.ua>
2022-04-10 10:16:18 +0300

    last

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const middlewares = jsonServer.defaults({
 const PORT = process.env.PORT || 3003;
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+  '/api/*':'/$1'
+}))
 server.use(router);
 server.listen(PORT, ()=> {
   console.log('Server is running');
